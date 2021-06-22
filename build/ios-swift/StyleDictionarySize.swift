02b198d494a3469e319d1e6b079243a4c3a749cd
balanier <balanier@users.noreply.github.com>
2021-06-22 23:08:03 +0000

    Update platform deliverables

--- build/ios-swift/StyleDictionarySize.swift
+++ build/ios-swift/StyleDictionarySize.swift
@@ -3,7 +3,7 @@
 // StyleDictionarySize.swift
 //
 // Do not edit directly
-// Generated on Tue, 22 Jun 2021 21:47:28 GMT
+// Generated on Tue, 22 Jun 2021 23:08:02 GMT
 //
 
 import UIKit
--- build/ios/StyleDictionarySize.h
+++ build/ios/StyleDictionarySize.h
@@ -2,7 +2,7 @@
 // StyleDictionarySize.h
 //
 // Do not edit directly
-// Generated on Tue, 22 Jun 2021 21:47:28 GMT
+// Generated on Tue, 22 Jun 2021 23:08:02 GMT
 //
 
 #import <Foundation/Foundation.h>
