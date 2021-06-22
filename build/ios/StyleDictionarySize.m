270c7b0bec74998ddf97906164f01afb1377b59e
balanier <balanier@users.noreply.github.com>
2021-06-22 23:09:32 +0000

    Update platform deliverables

--- build/ios/StyleDictionarySize.m
+++ build/ios/StyleDictionarySize.m
@@ -3,7 +3,7 @@
 // StyleDictionarySize.m
 //
 // Do not edit directly
-// Generated on Tue, 22 Jun 2021 23:08:02 GMT
+// Generated on Tue, 22 Jun 2021 23:09:31 GMT
 //
 
 #import "StyleDictionarySize.h"
